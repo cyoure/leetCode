8bd60327d23b7f598105f63521cc49f0305de852
cyoure <152484982+cyoure@users.noreply.github.com>
2026-04-22 00:30:54 +0900

    Time: 1 ms (76.85%), Space: 47 MB (11.26%) - LeetHub

---
+++ Java/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+         if (nums.length == 0) return 0;
+        
+        int k = 1;  // 첫 번째 원소는 항상 유일함
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
